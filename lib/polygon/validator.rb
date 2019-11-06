2974f570dbcdf47ecb4c84bf31e99f3be4bcebc2
Alexander Ross <alex@aross.se>
2019-11-06 16:39:21 +0100

    Initial commit
    Jira FL-120

---
+++ lib/polygon/validator.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'polygon/validator/version'
+
+module Polygon
+  module Validator
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
